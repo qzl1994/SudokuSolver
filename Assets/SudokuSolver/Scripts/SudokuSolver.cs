335dc3fd8c24fb53d223660458d295a035d8b8b1
qzl1994 <305165598@qq.com>
2017-01-06 12:00:30 +0800

    初始界面

---
+++ Assets/SudokuSolver/Scripts/SudokuSolver.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 数独解算器
+/// </summary>
+namespace SudokuSolver
+{
+    public class SudokuSolver : MonoBehaviour
+    {
+        public GameObject SudokuPanel;
+        public Button StartButton;
+
+        private int[,] Sudoku;
+
+        void Awake()
+        {
+
+            Object cellPrefab = Resources.Load("Prefabs/cell") as Object;
+
+            if (cellPrefab != null)
+            {
+                for(int i = 0; i < 9; i++)
+                {
+                    for(int j = 0; j < 9; j++)
+                    {
+                        GameObject cell = Instantiate(cellPrefab) as GameObject;
+                        cell.transform.SetParent(SudokuPanel.transform, false);
+                    }
+                }
+            }
+        }
+
+        void Start()
+        {
+            //StartButton.onClick.AddListener(Show);
+        }
+
+        public void Show()
+        {
+            
+        }
+
+    }
+}
